b4e5801da971429f423f8684a7efe519029eff8f
Andrej Hristov <andrejhris23@gmail.com>
2021-02-09 21:02:15 +0100

    onSubmit function updated

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -2,31 +2,31 @@ import React from 'react';
 import Axios from '../axios/axios_app';
 // import Axios from 'axios';
 
-const ApiService = () => {
+const ApiService = {
 
-    const getUserInterests = (userId) => {
+     getUserInterests = (userId) => {
         Axios.get(`/api/userInterests/all/byUser/${userId}`)
         .then(res => res.data())
         .catch(err => console.log(err));
-    }
+    },
 
-    const createUser = (user) => {
+    createUser = (user) => {
         Axios.post("/api/users/createUser", {
              body: user
         })
         .then(res => res.data())
         .catch(err => console.log(err));
-    }
+    },
 
-    const createUserInterest = (userInterest, userId) => {
+    createUserInterest = (userInterest, userId) => {
         Axios.post("/api/userInterests/createUserInterest", {
             body: {userInterest, userId}
         })
         .then(res => res.data())
         .catch(err => console.log(err));
-    }
+    },
 
-    const editUserInterests = (newUserInterest, userId) => {
+    editUserInterests = (newUserInterest, userId) => {
         Axios.patch("/api/userInterests/editUserInterest", {
             body: {newUserInterest, userId}
         })
